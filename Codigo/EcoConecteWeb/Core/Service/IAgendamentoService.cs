9727cede6d841fcdf485af8f313b606feea5893f
Lucas Werthein <105745101+LucasWertheinSantos@users.noreply.github.com>
2024-09-11 12:31:24 -0300

    Continuação na implementação

---
+++ Codigo/EcoConecteWeb/Core/Service/IAgendamentoService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IAgendamentoService
+    {
+        int Create();
+
+        int Edit();
+
+        int Delete();
+
+        Agendamento Get(int id);
+
+        IEnumerable<Agendamento> GetAll();
+
+        Agendamento GetById(int id);
+    }
+}
