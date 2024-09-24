8027e5721a8145bd2a95920b17cd1294a6ec7402
Carol <carolinevivaslins@gmail.com>
2024-09-24 10:36:08 -0300

    Criação do Service/IPessoaService.cs, PessoaDto.cs e Service/PessoaService.cs

---
+++ Codigo/EcoConecteWeb/Core/Service/IPessoaService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IPessoaService
+    {
+        uint Create(Pessoa pessoa);
+        void Edit(Pessoa pessoa);
+        void Delete(uint id);
+        Pessoa? Get(uint id);
+        IEnumerable<Pessoa> GetAll();
+        IEnumerable<PessoaDto> GetByName(string name);
+    }
+}
