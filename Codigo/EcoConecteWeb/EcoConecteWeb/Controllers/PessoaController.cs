0272fd8330dce2c2212be2feff29eb67303cf769
Carol <carolinevivaslins@gmail.com>
2024-09-24 11:22:27 -0300

    Implementação da classe Controllers/PessoaController.cs e PessoaViewModel.cs

---
+++ Codigo/EcoConecteWeb/EcoConecteWeb/Controllers/PessoaController.cs
@@ -0,0 +1,94 @@
+﻿using AutoMapper;
+using Core;
+using Core.Service;
+using EcoConecteWeb.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcoConecteWeb.Controllers
+{
+    public class PessoaController : Controller
+    {
+        private readonly IPessoaService _pessoaService;
+        private readonly IMapper _mapper;
+
+        public PessoaController(IPessoaService pessoaService, IMapper mapper)
+        {
+            _pessoaService = pessoaService;
+            _mapper = mapper;
+        }
+
+        // GET: Pessoa_Controller
+        public ActionResult Index()
+        {
+            var listaPessoas = _pessoaService.GetAll();
+            var listaPessoasModel = _mapper.Map<List<PessoaViewModel>>(listaPessoas);
+            return View(listaPessoasModel);
+        }
+
+        // GET: Pessoa_Controller/Details/5
+        public ActionResult Details(uint id)
+        {
+            Pessoa? pessoa = _pessoaService.Get(id);
+            PessoaViewModel pessoaModel = _mapper.Map<PessoaViewModel>(pessoa);
+            return View(pessoaModel);
+        }
+
+        // GET: Pessoa_Controller/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pessoa_Controller/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(PessoaViewModel pessoaModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var pessoa = _mapper.Map<Pessoa>(pessoaModel);
+                _pessoaService.Create(pessoa);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Pessoa_Controller/Edit/5
+        public ActionResult Edit(uint id)
+        {
+            Pessoa? pessoa = _pessoaService.Get(id);
+            PessoaViewModel pessoaModel = _mapper.Map<PessoaViewModel>(pessoa);
+            return View(pessoaModel);
+        }
+
+        // POST: Pessoa_Controller/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(uint id, PessoaViewModel pessoaModel)
+        {
+            if(ModelState.IsValid)
+            {
+                var pessoa = _mapper.Map<Pessoa>(pessoaModel);
+                _pessoaService.Edit(pessoa);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Pessoa_Controller/Delete/5
+        public ActionResult Delete(uint id)
+        {
+            Pessoa? pessoa = _pessoaService.Get(id);
+            PessoaViewModel pessoaModel = _mapper.Map<PessoaViewModel>(pessoa);
+            return View(pessoaModel);
+        }
+
+        // POST: Pessoa_Controller/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(uint id, PessoaViewModel pessoaModel)
+        {
+            _pessoaService.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
