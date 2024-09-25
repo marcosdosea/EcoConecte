98509e5fef29bb90e26e0959212a53616be61a07
Lucas Werthein <105745101+LucasWertheinSantos@users.noreply.github.com>
2024-09-25 12:28:54 -0300

    Recuperação dos arquivos do repositório.

---
+++ Codigo/EcoConecteWeb/Core/Dto/PessoaDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Dto
+{
+    public class PessoaDto
+    {
+        public uint IdPessoa { get; set; }
+
+        public string? Nome { get; set; }
+    }
+}
