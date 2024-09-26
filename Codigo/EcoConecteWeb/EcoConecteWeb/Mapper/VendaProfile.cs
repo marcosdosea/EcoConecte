43b61db0474aa3aad00eb40aabe576af617880d4
Matheus <matheuz14.al@gmail.com>
2024-09-25 22:47:47 -0300

    Closed #67

---
+++ Codigo/EcoConecteWeb/EcoConecteWeb/Mapper/VendaProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core;
+using EcoConecteWeb.Models;
+
+namespace EcoConecteWeb.Mapper
+{
+    public class VendaProfile : Profile
+    {
+        public VendaProfile() 
+        {
+            CreateMap<VendaViewModel, Vendamaterial>().ReverseMap();
+        }
+    }
+}
