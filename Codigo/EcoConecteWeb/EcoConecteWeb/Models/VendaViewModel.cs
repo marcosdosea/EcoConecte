78a8939953d3ed4a36ea0d72029217d32dbbf106
Matheus <matheuz14.al@gmail.com>
2024-09-25 22:23:40 -0300

    Closed #68

---
+++ Codigo/EcoConecteWeb/EcoConecteWeb/Models/VendaViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Org.BouncyCastle.Crypto.Digests;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+
+namespace EcoConecteWeb.Models
+{
+    public class VendaViewModel
+    {
+        [Key]
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        [Display(Name = "Código da venda")]
+        public uint Id { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        [Display(Name = "Tipo do material")]
+        public string Tipo { get; set; } = null!;
+
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        public decimal Valor { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        public decimal Quantidade { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        public DateTime Data { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        [Display(Name = "Código da cooperativa")]
+        public uint IdCooperativa { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        [Display(Name = "Código da pessoa")]
+        public uint IdPessoa { get; set; }
+    }
+}
