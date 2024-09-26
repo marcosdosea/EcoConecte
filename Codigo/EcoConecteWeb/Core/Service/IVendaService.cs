786f7d66f70d1a51b556c25241110a42574a3554
Matheus <matheuz14.al@gmail.com>
2024-09-25 21:55:50 -0300

    Closed #65

---
+++ Codigo/EcoConecteWeb/Core/Service/IVendaService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IVendaService
+    {
+        uint Create(Vendamaterial Venda);
+
+        void Update(Vendamaterial Venda);
+
+        bool Delete(uint Id);
+
+        IEnumerable<Vendamaterial> GetAll();
+
+        Vendamaterial? GetById(uint Id);
+    }
+}
