bd0804baabdaf99d31f4c097c33e4eb14bf50a45
Alexandre Moreira <xandemoreira1505@gmail.com>
2024-09-27 20:59:50 -0300

    Foram inseridos os ColetaProfile.cs, IColetaService, ColetaViewModel, ColetaService, Coleta

--- Codigo/EcoConecteWeb/Core/Coleta.cs
+++ Codigo/EcoConecteWeb/Core/Coleta.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Core;
 
-public partial class Rotinadecoletum
+public partial class Coleta
 {
     public uint Id { get; set; }
 
---
+++ Codigo/EcoConecteWeb/EcoConecteWeb/Models/ColetaViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EcoConecteWeb.Models
+{
+    public class ColetaViewModel
+    {
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public uint Id { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public DateTime Data { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [StringLength( 8,MinimumLength = 8,ErrorMessage ="O CEP deve conter 8 digitos")]
+        public string Cep { get; set; } = null!;
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string Logradouro { get; set; } = null!;
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [StringLength(4, MinimumLength = 4, ErrorMessage = "O Numero da casa deve conter 4 digitos")]
+        [Display(Name = "Número da Residencia")]
+        public string Numero da residencia { get; set; } = null!;
+
+
+        [AllowNull]
+        public string? Status { get; set; }
+    }
+}
