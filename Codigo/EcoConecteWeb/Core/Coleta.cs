959eb6474b41312ba1fd5601842c1ff78103427d
Alexandre Moreira <xandemoreira1505@gmail.com>
2024-09-27 22:57:34 -0300

    Foi adcionado O manter coleta

---
+++ Codigo/EcoConecteWeb/Core/Coleta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core;
+
+public partial class Coleta
+{
+    public uint Id { get; set; }
+
+    public string Cep { get; set; } = null!;
+
+    public string Logradouro { get; set; } = null!;
+
+    public int Numero { get; set; }
+
+    public DateTime DiaColeta { get; set; }
+
+    public DateTime? HorarioInicio { get; set; }
+
+    public DateTime? HorarioTermino { get; set; }
+
+    public uint IdCooperativa { get; set; }
+}
