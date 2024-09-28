c7e19afce34f825d728b1ab6560ea964b1bf8815
victorgabrielvieiraa <88777214+victorgabrielvieiraa@users.noreply.github.com>
2024-09-27 21:46:09 -0300

    Implementação do mapper Manter Cooperativa

---
+++ Codigo/EcoConecteWeb/EcoConecteWeb/Mapper/CooperativaProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core;
+using EcoConecteWeb.Models;
+
+namespace EcoConecteWeb.Mapper
+{
+    public class CooperativaProfile : Profile
+    {
+        public CooperativaProfile()
+        {
+            CreateMap<NoticiumViewModel, Noticium>().ReverseMap();
+        }
+    }
+}
