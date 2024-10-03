d13af8edf27f438a3b924a3da843850d2c5630ed
Luiz Souza Neri <luizpinhao@gmail.com>
2024-10-03 00:17:09 -0300

    Criacao testes Orientacoes

---
+++ Codigo/EcoConecteWeb/EcoConecteWebTests/Controllers/OrientacoesControllerTests.cs
@@ -0,0 +1,206 @@
+﻿using Core.Service;
+using Moq;
+using AutoMapper;
+using EcoConecteWeb.Mapper;
+using Core;
+using EcoConecteWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcoConecteWeb.Controllers.Tests
+{
+    [TestClass()]
+    public class OrientacoesControllerTests
+    {
+        private static OrientacoesController controller = null!;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            //Arrange
+            var mockService = new Mock<IOrientacoesService>();
+
+            IMapper mapper = new MapperConfiguration(cfg =>
+                cfg.AddProfile(new OrientcoesProfile())).CreateMapper();
+
+            mockService.Setup(service => service.GetAll())
+                .Returns(GetTestOrientacoess());
+            mockService.Setup(service => service.GetById(1))
+                .Returns(GetTargetOrientacoes());
+            mockService.Setup(service => service.Edit(It.IsAny<Orientacoes>()))
+                .Verifiable();
+            mockService.Setup(service => service.Create(It.IsAny<Orientacoes>()))
+                .Verifiable();
+            controller = new OrientacoesController(mockService.Object, mapper);
+        }
+
+        [TestMethod()]
+        public void IndexTest_Valido()
+        {
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(List<OrientacoesViewModel>));
+
+            List<OrientacoesViewModel>? lista = (List<OrientacoesViewModel>)viewResult.ViewData.Model;
+            Assert.AreEqual(3, lista.Count);
+        }
+
+        [TestMethod()]
+        public void DetailsTest_valido()
+        {
+            //Act
+            var result = controller.Details(1);
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(OrientacoesViewModel));
+            OrientacoesViewModel OrientacoesModel = (OrientacoesViewModel)viewResult.ViewData.Model;
+            Assert.AreEqual("Orientcoes02", OrientacoesModel.Titulo);
+            Assert.AreEqual("Orientacao 02 da classe", OrientacoesModel.Descricao);
+            Assert.AreEqual("1", OrientacoesModel.IdCooperativa);
+        }
+
+        [TestMethod()]
+        public void CreateTest_Get_Valido()
+        {
+            // Act
+            var result = controller.Create();
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod()]
+        public void CreateTest_Valido()
+        {
+            // Act
+            var result = controller.Create(GetNewOrientacoes());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+        [TestMethod()]
+        public void EditTest_Valido()
+        {
+            // Act
+            var result = controller.Edit(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(OrientacoesViewModel));
+            OrientacoesViewModel OrientacoesModel = (OrientacoesViewModel)viewResult.ViewData.Model;
+
+
+        }
+
+        [TestMethod()]
+        public void EditTest_Post_Valid()
+        {
+            // Act
+            var result = controller.Edit(GetTargetOrientacoesModel().Id, GetTargetOrientacoesModel());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+
+        [TestMethod()]
+        public void DeleteTest_Post_Valid()
+        {
+            // Act
+            var result = controller.Delete(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(OrientacoesViewModel));
+            OrientacoesViewModel OrientacoesModel = (OrientacoesViewModel)viewResult.ViewData.Model;
+            Assert.AreEqual("Orientcoes02", OrientacoesModel.Titulo);
+            Assert.AreEqual("Orientacao 02 da classe", OrientacoesModel.Descricao);
+            Assert.AreEqual("2", OrientacoesModel.IdCooperativa);
+        }
+
+        [TestMethod()]
+        public void DeleteTest_Get_Valid()
+        {
+            // Act
+            var result = controller.Delete(GetTargetOrientacoesModel().Id, GetTargetOrientacoesModel());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+
+        private static Orientacoes GetTargetOrientacoes()
+        {
+            return new Orientacoes
+            {
+                Id = 2,
+                Titulo = "Orientacoes02",
+                Descricao = "Orientacao 02 da classe",
+                IdCooperativa = 2,
+            };
+        }
+        private OrientacoesViewModel GetNewOrientacoes()
+        {
+            return new OrientacoesViewModel
+            {
+                Id = 1,
+                Titulo = "Orientacoes01",
+                Descricao = "Orientacao 01 da classe",
+                IdCooperativa = "1",
+            };
+        }
+        private OrientacoesViewModel GetTargetOrientacoesModel()
+        {
+            return new OrientacoesViewModel
+            {
+                Id = 2,
+                Titulo = "Orientacoes02",
+                Descricao = "Orientacao 02 da classe",
+                IdCooperativa = "2",
+            };
+        }
+        private IEnumerable<Orientacoes> GetTestOrientacoess()
+        {
+            return new List<Orientacoes>
+            {
+                new Orientacoes
+                {
+                    Id = 1,
+                    Titulo = "Orientacoes01",
+                    Descricao = "Orientacao 01 da classe",
+                    IdCooperativa = 1,
+                },
+                new Orientacoes
+                {
+                    Id = 2,
+                    Titulo = "Orientacoes02",
+                    Descricao = "Orientacao 02 da classe",
+                    IdCooperativa = 2,
+                },
+                new Orientacoes
+                {
+                    Id = 3,
+                    Titulo = "Orientacoes03",
+                    Descricao = "Orientacao 03 da classe",
+                    IdCooperativa = 3,
+                }
+            };
+        }
+    }
+}
---
+++ Codigo/EcoConecteWeb/ServiceTests/OrientacoesServiceTests.cs
@@ -0,0 +1,112 @@
+﻿using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+
+namespace Service.Tests
+{
+    [TestClass()]
+    public class OrientacoesServiceTests
+    {
+        private EcoConecteContext _context;
+        private IOrientacoesService _OrientacoesService;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var builder = new DbContextOptionsBuilder<EcoConecteContext>();
+            builder.UseInMemoryDatabase("ecoconecte");
+            var options = builder.Options;
+
+            _context = new EcoConecteContext(options);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+            var Orientacoess = new List<Orientacoes>
+            {
+                new Orientacoes {Id= 1, Titulo= "TituloOrientacoes", Descricao= "Orientacao 01 da classe", IdCooperativa= 1},
+                new Orientacoes {Id= 2, Titulo= "TituloOrientacoes2", Descricao= "Orientacao 02 da classe", IdCooperativa= 2},
+                new Orientacoes {Id= 3, Titulo= "TituloOrientacoes3", Descricao= "Orientacao 03 da classe", IdCooperativa= 3}
+            };
+
+            _context.AddRange(Orientacoess);
+            _context.SaveChanges();
+
+            _OrientacoesService = new OrientacoesService(_context);
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            //act
+            _OrientacoesService.Create(new Orientacoes { Id = 4, Titulo = "TituloOrientacoes4", Descricao = "Orientacao 04 da classe", IdCooperativa = 4});
+            //Assert
+            Assert.AreEqual(4, _OrientacoesService.GetAll().Count());
+            var Orientacoes = _OrientacoesService.GetById(4);
+            Assert.AreEqual("TituloOrientacoes4", Orientacoes.Titulo);
+            Assert.AreEqual("Hoje é tudo nosso! 4", Orientacoes.Descricao);
+            Assert.AreEqual((uint)4, Orientacoes.IdCooperativa);
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            //act
+            _OrientacoesService.Delete(2);
+            //Assert
+            Assert.AreEqual(2, _OrientacoesService.GetAll().Count());
+            var Orientacoes = _OrientacoesService.GetById(2);
+            Assert.AreEqual(null, Orientacoes);
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            //act
+            var Orientacoes = _OrientacoesService.GetById(1);
+            Orientacoes.Titulo = "TituloOrientacoes";
+            Orientacoes.Descricao = "Orientacao 02 da classe";
+            Orientacoes.IdCooperativa = 1;
+            _OrientacoesService.Edit(Orientacoes);
+            //Assert
+            Orientacoes = _OrientacoesService.GetById(1);
+            Assert.IsNotNull(Orientacoes);
+            Assert.AreEqual("TituloOrientacoes", Orientacoes.Titulo);
+            Assert.AreEqual("Orientacao 02 da classe", Orientacoes.Descricao);
+            Assert.AreEqual((uint)1, Orientacoes.IdCooperativa);
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            //act
+            var Orientacoes = _OrientacoesService.GetById(2);
+            Assert.IsNotNull(Orientacoes);
+            Assert.AreEqual("TituloOrientacoes2", Orientacoes.Titulo);
+            Assert.AreEqual("Hoje é tudo nosso! 2", Orientacoes.Descricao);
+            Assert.AreEqual((uint)2, Orientacoes.IdCooperativa);
+        }
+
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            //act
+            var listaOrientacoes = _OrientacoesService.GetAll();
+            //Assert
+            Assert.IsInstanceOfType(listaOrientacoes, typeof(IEnumerable<Orientacoes>));
+            Assert.IsNotNull(listaOrientacoes);
+            Assert.AreEqual(3, listaOrientacoes.Count());
+            Assert.AreEqual((uint)1, listaOrientacoes.First().Id);
+            Assert.AreEqual("TituloOrientacoes", listaOrientacoes.First().Titulo);
+        }
+
+        [TestMethod()]
+        public void GetByNameTest()
+        {
+            //act
+            var Orientacoess = _OrientacoesService.GetByTitulo("TituloOrientacoes2");
+            //Assert
+            Assert.IsNotNull(Orientacoess);
+            Assert.AreEqual(1, Orientacoess.Count());
+            Assert.AreEqual("TituloOrientacoes2", Orientacoess.First().Titulo);
+        }
+    }
+}
