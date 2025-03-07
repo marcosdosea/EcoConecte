aeff3c955fdb203d1c57b7ae5bbed10828f5cde1
Lucas Werthein <lucaswerthaine@gmail.com>
2025-03-07 17:44:45 -0300

    Ajuestes nos menus de login da cooperativa e cooperado, criação da view AgendamentoCoop.

---
+++ Codigo/EcoConecteWeb/EcoConecteWeb/Views/Cooperado/AgendamentoCoop.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EcoConecteWeb.Views.Cooperado
+{
+    public class AgendamentoCoopModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
