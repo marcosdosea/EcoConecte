a1c82b6f61cadb4ef5cd2e69d6675b27df646a72
Lucas Werthein <lucaswerthaine@gmail.com>
2025-03-08 01:55:26 -0300

    View das Coletas atreladas a cooperativa e ao Cooperado criada e incompleta

--- Codigo/EcoConecteWeb/EcoConecteWeb/Views/Coleta/AgendamentoCoop.cshtml.cs
+++ Codigo/EcoConecteWeb/EcoConecteWeb/Views/Coleta/AgendamentoCoop.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace EcoConecteWeb.Views.Cooperado
+namespace EcoConecteWeb.Views.Coleta
 {
     public class AgendamentoCoopModel : PageModel
     {
