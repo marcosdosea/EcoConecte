4405a2a01b7cdaf8caa70475af42087ab8534ab2
Luiz Souza Neri <luizpinhao@gmail.com>
2025-03-31 04:15:27 -0300

    add views

---
+++ Codigo/EcoConecteWeb/EcoConecteWeb/Views/AdmIdentity/UserRoles.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EcoConecteWeb.Views.AdmIdentity
+{
+    public class UserRolesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Codigo/EcoConecteWeb/EcoConecteWeb/Views/AdmIdentity/Users.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EcoConecteWeb.Views.AdmIdentity
+{
+    public class UsersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
