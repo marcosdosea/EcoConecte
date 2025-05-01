3e756ddc5659c26c9d77e4400d3ec0130ca591a4
Luiz Souza Neri <luizpinhao@gmail.com>
2025-05-01 00:40:42 -0300

    Ajustes de vendas e orientcoes admcooperativa e filtros em local melhor

---
+++ Codigo/EcoConecteWeb/EcoConecteWeb/Views/Shared/_FiltrosVendas.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EcoConecteWeb.Views.Shared
+{
+    public class _FiltrosVendasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
